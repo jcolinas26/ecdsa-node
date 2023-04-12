f364f6b1d6ec7f50545a32b5eb59f4feb2f6ec9f
JC <jcolinas26@gmail.com>
2023-04-12 19:42:15 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+//const { useState } = require("react");
+
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex, hexToBytes } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+const { sha256 } = require("ethereum-cryptography/sha256");
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "baf86a977784522aa8b707fe64a391dd1400d871": 1000,
+  "63a9481f7b7e64bc8ec0a545043c5bd21fdccf07": 500,
+  "92b21cce358e1feca059a7dbd4d07858aa9ef811": 750,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,19 +26,44 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, signature, recoverKey, msgHash } = req.body;
+  console.log('SIGNATURE1: ' + signature);
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+  //verify signature before check balances-----
+  console.log('PREVIOUS PUBLIC KEY ON SERVER');
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
+  const msgHash2 = keccak256(utf8ToBytes(msgHash));
+  let publicKey = secp.recoverPublicKey(msgHash2, signature, recoverKey);
+  console.log('PUBLIC KEY ON SERVER: ' + publicKey);
+  console.log('SENDER: ' + sender);
+
+  const rest_Pkey = publicKey.slice(1);
+  const hash = keccak256(rest_Pkey);
+  const address = toHex(hash.slice(-20));
+  console.log('ADDRESS: ' + address);//getting the address from the public key
+
+  if (address != sender) {
+
+    res.status(401).send({
+      message:
+        "Sender is not authorised for this transaction! Invalid Signature",
+    });
   } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
+
   }
+
 });
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
